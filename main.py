1f2a1c9046c536225a203a1c053c9d0a6f0552a4
aksss7 <aksss7.kg@gmail.com>
2023-07-20 18:21:49 +0600

    tiles count

--- main.py
+++ main.py
@@ -67,8 +67,32 @@ def colorize_tiles(tiles, palette):
 
 letter_to_color = {v: k for k, v in color_to_letter.items()} 
 
+def count_consecutive_same_elements(lst):
+    result = []
+    prev_element = None
+    count = 1
+    for element in lst:
+        if element == prev_element:
+            count += 1
+        else:
+            if prev_element is not None:
+                result.extend([prev_element]*count)
+            prev_element = element
+            count = 1
+    if prev_element is not None:
+        result.extend([prev_element]*count)
+
+    # Обработка случая, когда весь блок одного цвета
+    if len(set(result)) == 1:
+        return list(range(1, len(result) + 1))
+
+    return result
+
+
 def blocks_to_pdf(blocks, filename, blocks_per_page, logo_path, logo_width, logo_height, logo_x, logo_y):
+    # Создаем экземпляр класса PDF
     pdf = PDF(orientation='P', unit='mm', format='A4')
+    # Определяем размеры и расположение элементов на странице
     blocks_per_row = 3
     blocks_per_column = blocks_per_page // blocks_per_row
     pages = (len(blocks) - 1) // blocks_per_page + 1
@@ -76,104 +100,126 @@ def blocks_to_pdf(blocks, filename, blocks_per_page, logo_path, logo_width, logo
     group_width = group_height = min((pdf.w - 30) / blocks_per_row, (pdf.h - 30) / blocks_per_column)
     spacing = 15 
 
-    tile_size = (group_height - 20) / max(9, 15)  # Assuming 9 tiles per row and 15 per column in a block
-    tile_spacing = 0.5  # spacing between tiles
+    tile_size = (group_height - 20) / max(9, 15)  # Предполагая 9 плиток в строке и 15 в столбце в блоке
+    tile_spacing = 0.5  # Промежуток между плитками
 
-    font_size = 4  # Adjust the font size as needed
+    font_size = 4  # Регулируйте размер шрифта по мере необходимости
 
-    # Define the y offset to move the entire section down
+    # Определяем смещение по оси Y для перемещения всего блока вниз
     y_offset = 20
 
     for p in range(pages):
         pdf.add_page()
 
-        # Move the entire section down
+        # Перемещаем блок вниз
         pdf.set_y(pdf.get_y() + y_offset)
 
-        # Add the block range in the top right corner
+        # Добавляем диапазон блоков в верхнем правом углу
         block_range = f"{blocks_per_page * p + 1}-{min(blocks_per_page * (p + 1), len(blocks))}"
         pdf.set_font("Arial", style='B', size=35)
-        pdf.set_text_color(0, 0, 0)  # black color
+        pdf.set_text_color(0, 0, 0)  # черный цвет
         pdf.cell(0, -30, txt=block_range, align="R", ln=True)
 
-        # Add the logo
+        # Добавляем логотип
         pdf.image(logo_path, x=logo_x, y=logo_y, w=logo_width, h=logo_height)
 
         for i in range(blocks_per_page * p, min(blocks_per_page * (p + 1), len(blocks))):
             block = blocks[i]
+            counted_block = count_consecutive_same_elements(block)
 
             group_x = (i % blocks_per_row) * group_width + spacing
             group_y = ((i % blocks_per_page) // blocks_per_row) * group_height + spacing
 
-            # Move the entire block down
+            # Перемещаем весь блок вниз
             group_y += y_offset
 
-            # Draw colored tiles first
+            counter = 0
+            previous_tile = None
+            # Рисуем цветные плитки
             for t, letter in enumerate(block):
                 x = group_x + 20 + (t % 9) * (tile_size + tile_spacing)
                 y = group_y + 10 + (t // 9) * (tile_size + tile_spacing)
-                color = letter_to_color[letter]   # Fix here, map letter back to color
-                
-                # Draw the tile
-                pdf.set_fill_color(*color)
-                pdf.rounded_rect(x, y, tile_size, tile_size, 0.5, 'F')  # 2mm rounded corners
+                color = letter_to_color[letter]   # Исправление здесь, сопоставляем букву с цветом
 
-            # Draw letters on the tiles
+                # Рисуем плитку
+                pdf.set_fill_color(*color)
+                pdf.rounded_rect(x, y, tile_size, tile_size, 0.5, 'F')  # Закругленные углы 2 мм
+
+            # Рисуем номера на плитках
+            counter = 1
+            previous_tile = None
+            for t, letter in enumerate(block):
+                x = group_x + 19.5 + (t % 9) * (tile_size + tile_spacing)
+                y = group_y + 7 + (t // 9) * (tile_size + tile_spacing)
+
+                # Сбрасываем счетчик, когда цвет плитки меняется
+                if previous_tile != letter:
+                    counter = 1
+                previous_tile = letter
+
+                # Рисуем номер в верхнем правом углу плитки
+                pdf.set_font("Arial", style='B', size=font_size - 1)
+                pdf.set_text_color(129, 129, 129)  # черный цвет
+                pdf.set_xy(x + tile_size - pdf.get_string_width(str(counter)) - 1, y + 1)
+                pdf.cell(0, 0, str(counter), ln=False)
+
+                # Увеличиваем счетчик
+                counter += 1
+
+            # Рисуем буквы на плитках
             for t, letter in enumerate(block):
                 x = group_x + 18.5 + (t % 9) * (tile_size + tile_spacing)
                 y = group_y + 6 + (t // 9) * (tile_size + tile_spacing)
 
-                # Draw the letter on the tile
+                # Рисуем букву на плитке
                 pdf.set_font("Arial", style='B', size=font_size)
-                pdf.set_text_color(129, 129, 129)  # black color
+                pdf.set_text_color(129, 129, 129)  # серый цвет
                 pdf.set_xy(x + tile_size / 2 - pdf.get_string_width(letter) / 2, y + tile_size / 2 + font_size / 2)
                 pdf.cell(0, 0, letter, ln=False)
 
-
-
-            # Draw tile numbers (top side)
+            # Рисуем номера плиток (верхняя сторона)
             pdf.set_font("Arial", style='B', size=font_size)
-            pdf.set_text_color(0, 0, 0)  # black color
+            pdf.set_text_color(0, 0, 0)  # черный цвет
             for t in range(9):
                 x = group_x + 19 + (t * (tile_size + tile_spacing)) + (tile_size - pdf.get_string_width(str(t + 1))) / 2
                 y = group_y + 10 - font_size
                 pdf.set_xy(x, y)
                 pdf.cell(0, 0, str(t + 1), ln=False)
 
-            # Draw tile numbers (bottom side)
+            # Рисуем номера плиток (нижняя сторона)
             pdf.set_font("Arial", style='B', size=font_size)
-            pdf.set_text_color(0, 0, 0)  # black color
+            pdf.set_text_color(0, 0, 0)  # черный цвет
             for t in range(9):
                 x = group_x + 19 + (t * (tile_size + tile_spacing)) + (tile_size - pdf.get_string_width(str(t + 1))) / 2
                 y = group_y + 8 + (15 * (tile_size + tile_spacing))
                 pdf.set_xy(x, y)
                 pdf.cell(0, 0, str(t + 1), ln=False)
 
-            # Draw tile numbers (left side)
+            # Рисуем номера плиток (левая сторона)
             pdf.set_font("Arial", style='B', size=font_size)
-            pdf.set_text_color(0, 0, 0)  # black color
+            pdf.set_text_color(0, 0, 0)  # черный цвет
             for t in range(15):
                 x = group_x + 18 - pdf.get_string_width(str(t + 1))
                 y = group_y + 9.5 + (t * (tile_size + tile_spacing)) + (tile_size - font_size) / 2
                 pdf.set_xy(x, y)
                 pdf.cell(0, 0, str(t + 1), ln=False)
 
-            # Draw tile numbers (right side)
+            # Рисуем номера плиток (правая сторона)
             pdf.set_font("Arial", style='B', size=font_size)
-            pdf.set_text_color(0, 0, 0)  # black color
+            pdf.set_text_color(0, 0, 0)  # черный цвет
             for t in range(15):
                 x = group_x + 19 + 9 * (tile_size + tile_spacing)
                 y = group_y + 9.5 + (t * (tile_size + tile_spacing)) + (tile_size - font_size) / 2
                 pdf.set_xy(x, y)
                 pdf.cell(0, 0, str(t + 1), ln=False)
 
-            # Draw line
-            pdf.set_draw_color(0, 0, 0)  # color for the line
-            pdf.set_line_width(0.7)  # set line thickness to 2mm
+            # Рисуем линию
+            pdf.set_draw_color(0, 0, 0)  # цвет линии
+            pdf.set_line_width(0.7)  # толщина линии 2 мм
             line_length = group_height - 6
             pdf.line(group_x + 15, group_y + 8, group_x + 15, group_y + line_length)
 
-            # Draw number with a cell to control the height
+            # Рисуем номер с ячейкой для контроля высоты
             pdf.set_font("Arial", style='B', size=16)
 
             number_length = len(str(i+1))
@@ -188,8 +234,7 @@ def blocks_to_pdf(blocks, filename, blocks_per_page, logo_path, logo_width, logo
 
     pdf.output(filename)
 
-
-image_path = 'photo.png'
+image_path = 'men.png'
 logo_path = 'self-pixel.png'
 logo_width = 50
 logo_height = 20
